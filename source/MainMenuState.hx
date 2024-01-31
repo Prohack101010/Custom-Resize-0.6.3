403b2dcfc115d3cf28847092daceee74db2717ae
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2024-02-01 01:52:56 +0300

    Initial commit

---
+++ source/MainMenuState.hx
@@ -0,0 +1,360 @@
+package;
+
+import flixel.util.FlxTimer;
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.effects.FlxFlicker;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import lime.app.Application;
+import Achievements;
+import editors.MasterEditorMenu;
+import flixel.input.keyboard.FlxKey;
+import haxe.Json;
+
+using StringTools;
+
+typedef MenuData =
+{
+    optionScroll:Float,
+    bgScroll:Float,
+    scale:Float,
+
+    optionPosition:Array<Float>,
+    optionHeight:Float,
+    optionCenter:Bool,
+    disable_editor:Bool,
+
+    addOption:Array<String>,
+    addText:Array<String>
+}
+class MainMenuState extends MusicBeatState
+{
+	public static var curSelected:Int = 0;
+	public static var launchChance:Dynamic = null;
+
+	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
+	public static var extraKeysVersion:String = '0.3';
+
+	var menuItems:FlxTypedGroup<FlxSprite>;
+	private var camGame:FlxCamera;
+	private var camAchievement:FlxCamera;
+
+    //LIKE SERIOUSLY IGNORE IT
+    var menuJSON:MenuData;
+
+	var magenta:FlxSprite;
+	var camFollow:FlxObject;
+	var camFollowPos:FlxObject;
+	var debugKeys:Array<FlxKey>;
+
+	override function create()
+	{
+		#if MODS_ALLOWED
+		Paths.pushGlobalMods();
+		#end
+		WeekData.loadTheFirstEnabledMod();
+
+		// Updating Discord Rich Presence
+		#if desktop
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+		debugKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_1'));
+
+		camGame = new FlxCamera();
+		camAchievement = new FlxCamera();
+		camAchievement.bgColor.alpha = 0;
+
+		FlxG.cameras.reset(camGame);
+		FlxG.cameras.add(camAchievement, false);
+		FlxG.cameras.setDefaultDrawTarget(camGame, true);
+
+		transIn = FlxTransitionableState.defaultTransIn;
+		transOut = FlxTransitionableState.defaultTransOut;
+
+		persistentUpdate = persistentDraw = true;
+        menuJSON = Json.parse(Paths.getTextFromFile('images/mainmenu/mainmenu.json'));
+
+		var yScroll:Float = (menuJSON.addOption.length - 4) * menuJSON.bgScroll;
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+		bg.scrollFactor.set(0, yScroll);
+		bg.setGraphicSize(Std.int(bg.width * 1.175));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+
+		camFollow = new FlxObject(0, 0, 1, 1);
+		camFollowPos = new FlxObject(0, 0, 1, 1);
+		add(camFollow);
+		add(camFollowPos);
+
+		magenta = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
+		magenta.scrollFactor.set(0, yScroll);
+		magenta.setGraphicSize(Std.int(magenta.width * 1.175));
+		magenta.updateHitbox();
+		magenta.screenCenter();
+		magenta.visible = false;
+		magenta.antialiasing = ClientPrefs.globalAntialiasing;
+		magenta.color = 0xFFfd719b;
+		add(magenta);
+		
+		// magenta.scrollFactor.set();
+
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
+
+		for (i in 0...menuJSON.addOption.length)
+		{
+            var offset:Float = 108 - (Math.max(menuJSON.addOption.length, 4) - 4) * 80;
+			var menuItem:FlxSprite = new FlxSprite(menuJSON.optionPosition[0], menuJSON.optionPosition[1] + (i * menuJSON.optionHeight) + offset);
+			menuItem.scale.x = menuJSON.scale;
+			menuItem.scale.y = menuJSON.scale;
+			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + menuJSON.addOption[i]);
+			menuItem.animation.addByPrefix('idle', menuJSON.addOption[i] + " basic", 24);
+			menuItem.animation.addByPrefix('selected', menuJSON.addOption[i] + " white", 24);
+			menuItem.animation.play('idle');
+			menuItem.ID = i;
+			menuItems.add(menuItem);
+			var scr:Float = (menuJSON.addOption.length - 4) * menuJSON.optionScroll;
+			if(menuJSON.addOption.length < 6) scr = 0;
+			menuItem.scrollFactor.set(0, scr);
+			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
+			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
+			menuItem.updateHitbox();
+		}
+
+		if (menuJSON.optionCenter)
+		{
+    		menuItems.forEach(function(spr:FlxSprite)
+    		{
+    		spr.screenCenter(X);
+    		});
+		}
+
+		FlxG.camera.follow(camFollowPos, null, 1);
+		
+		if (FlxG.save.data.firstTimeUsing == null) {
+			FlxG.save.data.firstTimeUsing = true;
+		}
+
+		for (i in 0...menuJSON.addText.length) {
+			var versionShit:FlxText = new FlxText(12, (FlxG.height - 24) - (18 * i), 0, menuJSON.addText[i], 12);
+			versionShit.scrollFactor.set();
+			versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			add(versionShit);
+		}
+
+		// NG.core.calls.event.logEvent('swag').send();
+
+		changeItem();
+
+		#if ACHIEVEMENTS_ALLOWED
+		Achievements.loadAchievements();
+		var leDate = Date.now();
+		if (leDate.getDay() == 5 && leDate.getHours() >= 18) {
+			var achieveID:Int = Achievements.getAchievementIndex('friday_night_play');
+			if(!Achievements.isAchievementUnlocked(Achievements.achievementsStuff[achieveID][2])) { //It's a friday night. WEEEEEEEEEEEEEEEEEE
+				Achievements.achievementsMap.set(Achievements.achievementsStuff[achieveID][2], true);
+				giveAchievement();
+				ClientPrefs.saveSettings();
+			}
+		}
+		#end
+
+        #if mobile
+            if (menuJSON.disable_editor == true)
+            {
+                addVirtualPad(UP_DOWN,A_B_E);
+            
+            }
+            else
+            {
+                addVirtualPad(UP_DOWN,A_B);
+            }
+        #end
+
+		super.create();
+	}
+
+	function qatarShit():String {
+		var leGoal = new Date(2022, 11, 21, 12, 0, 0).getTime();
+
+		var second = 1000;
+		var minute = second * 60;
+		var hour = minute * 60;
+		var day = hour * 24;
+
+		var leDate = Date.now().getTime();
+		var timeLeft = leGoal - leDate;
+
+		var shitArray:Array<Dynamic> = [
+			Math.floor(timeLeft / (day)),
+         	Math.floor((timeLeft % (day)) / (hour)),
+			Math.floor((timeLeft % (hour)) / (minute)),
+        	Math.floor((timeLeft % (minute)) / second)
+		];
+
+		var zeroShitArray:Array<String> = ["day","hour","minute","second"];
+		
+		var leftTime:String = "";
+
+		for (i in 0...shitArray.length) {
+			if (shitArray[i] < 10) {
+				zeroShitArray[i] = '0' + shitArray[i];
+			} else zeroShitArray[i] = '' + shitArray[i];
+			var dosPuntos:String = (i > 0 && i < shitArray.length) ? ":" : "";
+
+			leftTime += dosPuntos + zeroShitArray[i];
+		}
+
+		return leftTime;
+	}
+
+	#if ACHIEVEMENTS_ALLOWED
+	// Unlocks "Freaky on a Friday Night" achievement
+	function giveAchievement() {
+		add(new AchievementObject('friday_night_play', camAchievement));
+		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		trace('Giving achievement "friday_night_play"');
+	}
+	#end
+
+	var selectedSomethin:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.8)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+			if(FreeplayState.vocals != null) FreeplayState.vocals.volume += 0.5 * elapsed;
+		}
+
+		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
+		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+
+		if (!selectedSomethin)
+		{
+			if (controls.UI_UP_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(-1);
+			}
+
+			if (controls.UI_DOWN_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(1);
+			}
+
+			if (controls.BACK)
+			{
+				selectedSomethin = true;
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new TitleState());
+			}
+
+			if (controls.ACCEPT)
+			{
+				if (menuJSON.addOption[curSelected] == 'donate')
+				{
+					CoolUtil.browserLoad('https://ninja-muffin24.itch.io/funkin');
+				}
+				else
+				{
+					selectedSomethin = true;
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+					FlxG.mouse.visible = false;
+
+					if(ClientPrefs.flashing) FlxFlicker.flicker(magenta, 1.1, 0.15, false);
+
+					menuItems.forEach(function(spr:FlxSprite)
+					{
+						if (curSelected != spr.ID)
+						{
+							FlxTween.tween(spr, {alpha: 0}, 0.4, {
+								ease: FlxEase.quadOut,
+								onComplete: function(twn:FlxTween)
+								{
+									spr.kill();
+								}
+							});
+						}
+						else
+						{
+							FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
+							{
+								var daChoice:String = menuJSON.addOption[curSelected];
+
+								switch (daChoice)
+								{
+									case 'story_mode':
+										MusicBeatState.switchState(new StoryMenuState());
+									case 'freeplay':
+										MusicBeatState.switchState(new FreeplayState());
+									#if MODS_ALLOWED
+									case 'mods':
+										MusicBeatState.switchState(new ModsMenuState());
+									#end
+									case 'awards':
+										MusicBeatState.switchState(new AchievementsMenuState());
+									case 'credits':
+										MusicBeatState.switchState(new CreditsState());
+									case 'options':
+										LoadingState.loadAndSwitchState(new options.OptionsState());
+								}
+							});
+						}
+					});
+				}
+			}
+		}
+	if (menuJSON.disable_editor == true)
+	{
+		if (FlxG.keys.anyJustPressed(debugKeys) #if android || _virtualpad.buttonE.justPressed #end)
+		{
+			selectedSomethin = true;
+			MusicBeatState.switchState(new MasterEditorMenu());
+		}
+	}
+		super.update(elapsed);
+	}
+
+	function changeItem(huh:Int = 0)
+	{
+		curSelected += huh;
+
+		if (curSelected >= menuItems.length)
+			curSelected = 0;
+		if (curSelected < 0)
+			curSelected = menuItems.length - 1;
+
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.animation.play('idle');
+			spr.updateHitbox();
+
+			if (spr.ID == curSelected)
+			{
+				spr.animation.play('selected');
+				var add:Float = 0;
+				if(menuItems.length > 4) {
+					add = menuItems.length * 8;
+				}
+				camFollow.setPosition(spr.getGraphicMidpoint().x, spr.getGraphicMidpoint().y - add);
+				spr.centerOffsets();
+			}
+		});
+	}
+}
---
+++ source/android/FlxHitbox.hx
@@ -0,0 +1,85 @@
+package android;
+
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxSpriteGroup;
+import flixel.ui.FlxButton;
+import flixel.FlxSprite;
+
+class FlxHitbox extends FlxSpriteGroup {
+	public var hitbox:FlxSpriteGroup;
+	public var hint:FlxSpriteGroup;
+	public var buttonLeft:FlxButton;
+	public var buttonDown:FlxButton;
+	public var buttonUp:FlxButton;
+	public var buttonRight:FlxButton;
+	public var buttonHintLeft:FlxSprite;
+	public var buttonHintDown:FlxSprite;
+	public var buttonHintUp:FlxSprite;
+	public var buttonHintRight:FlxSprite;
+
+	public function new() {
+		super();
+
+		hitbox = new FlxSpriteGroup();
+		hint = new FlxSpriteGroup();
+
+		buttonLeft = new FlxButton(0, 0);
+		buttonDown = new FlxButton(0, 0);
+		buttonUp = new FlxButton(0, 0);
+		buttonRight = new FlxButton(0, 0);
+
+        buttonHintLeft = new FlxSprite(0, 0);
+        buttonHintDown = new FlxSprite(0, 0);
+        buttonHintUp = new FlxSprite(0, 0);
+        buttonHintRight = new FlxSprite(0, 0);
+
+		hitbox.add(add(buttonLeft = createHitbox(0, 0, 0xC457D3)));
+		hitbox.add(add(buttonDown = createHitbox(320, 0, 0x00DAFF)));
+		hitbox.add(add(buttonUp = createHitbox(640, 0, 0x00FF00)));
+		hitbox.add(add(buttonRight = createHitbox(960, 0, 0xFF0000)));
+
+		hint.add(add(buttonHintLeft = createHitboxHint(0, 0, 0xC457D3)));
+		hint.add(add(buttonHintDown = createHitboxHint(320, 0, 0x00DAFF)));
+		hint.add(add(buttonHintUp = createHitboxHint(640, 0, 0x00FF00)));
+		hint.add(add(buttonHintRight = createHitboxHint(960, 0, 0xFF0000)));
+	}
+
+    public function createHitbox(x:Float = 0, y:Float = 0, colors:Int = 0xFFFFFF) {
+        var button:FlxButton = new FlxButton(x,y);
+        button.loadGraphic(Paths.image('androidcontrols/hitbox'));
+        button.updateHitbox();
+        button.color = colors;
+        button.alpha = 0;
+        add(button);
+
+        button.onOut.callback = function() {button.alpha = 0;}
+        button.onDown.callback = function() {button.alpha = ClientPrefs.mobileControlOpacity;}
+        button.onUp.callback = function() {button.alpha = 0;}
+
+        return button;
+    }
+
+    public function createHitboxHint(x:Float = 0, y:Float = 0, colors:Int = 0xFFFFFF) {
+        var buttonHint:FlxSprite = new FlxSprite(x,y);
+        buttonHint.loadGraphic(Paths.image('androidcontrols/hint'));
+		if (ClientPrefs.hideHint) {
+            buttonHint.alpha = 0;
+		} else {
+            buttonHint.alpha = ClientPrefs.mobileControlOpacity;
+		}
+		buttonHint.color = colors;
+        add(buttonHint);
+
+        return buttonHint;
+    }
+
+	override public function destroy():Void {
+		super.destroy();
+
+		buttonLeft = null;
+		buttonDown = null;
+		buttonUp = null;
+		buttonRight = null;
+	}
+}
